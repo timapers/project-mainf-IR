a9275b02b20402ea129f2ae9ed9fd140cf554ee9
morganlogghe <morgan.logghe@student.uantwerpen.be>
2022-11-27 16:49:29 +0100

    tf idf

--- src/vectorization.py
+++ src/vectorization.py
@@ -8,20 +8,21 @@ directory = '../data'
 documents = []
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
-    doc = []
     if os.path.isfile(f):
         file = open(f, 'r')
         lines = file.readlines()
         for line in lines:
-            doc.append(line)
+            print(line)
+            documents.append(line)
         file.close()
-    documents.append(doc)
+print(documents)
 
 #https://towardsdatascience.com/natural-language-processing-feature-engineering-using-tf-idf-e8b9d00e7e76
-stopwords.words('english')
-vectorizer = TfidfVectorizer(stopwords=stopwords)
+stopwords = stopwords.words('english')
+vectorizer = TfidfVectorizer(stop_words=stopwords)
 vectors = vectorizer.fit_transform(documents)
 feature_names = vectorizer.get_feature_names()
 dense = vectors.todense()
 denselist = dense.tolist()
 df = pd.DataFrame(denselist, columns=feature_names)
+print(df)
